b321cc110dc72f1b1975d74bd075dccfc75f6ec5
Gabriel <advspace.sistemas@gmail.com>
2024-12-11 12:22:59 -0400

    fix

--- src/@staticData/farm.ts
+++ src/@staticData/farm.ts
@@ -4,9 +4,11 @@ import Image from "@/public/images/staticDataImage/farm.jpg";
 export const FarmStaticData: FarmProps = {
   id: "1",
   name: "Fazenda Boa Vista",
+  title: "Fazenda Boa Vista",
   city: "Sinop",
   state: "MT",
   image: Image,
+  logo: Image,
   startDate: new Date(),
   lastUpdate: new Date(),
 };
--- src/@types/farm.ts
+++ src/@types/farm.ts
@@ -2,10 +2,12 @@ import { StaticImageData } from "next/image";
 
 export interface FarmProps {
   id: string;
+  name: string;
   title: string;
   city: string;
   state: string;
   startDate: Date;
   lastUpdate: Date;
+  image: string | StaticImageData;
   logo: string | StaticImageData;
 }
