b227a4969bfa7c2cf1f74d318aa656c84fc749ae
Gabriel <119818500+ExecutivosDigital@users.noreply.github.com>
2024-12-18 08:29:30 -0400

    deploy

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,6 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
 import avatar3 from "@/public/images/avatar/avatar-3.jpg";
-
 export const authOptions = {
   providers: [
     GoogleProvider({
